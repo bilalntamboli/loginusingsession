9b8d6070748e07d4119d23409db2e0fd6fe14cff
Bilal Tamboli <bilaltamboli@protonmail.com>
2021-03-22 15:17:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -54,29 +54,10 @@ if(@$_POST['submit1'])
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Login</title>
   </head>
   <body>
-    <!--Navbar-->
-
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">VPCSC</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-        
-        <li class="nav-item">
-          <a class="nav-link active" href="#">LOGIN</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">SIGNUP</a>
-        </li>
-    </div>
-  </div>
-</nav>
+  
  
  <!-- Login Form-->
 
